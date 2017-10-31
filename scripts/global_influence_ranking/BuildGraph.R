864306b9d72d269568509b0d3f845be5b166dd90
quepas <kiepas@agh.edu.pl>
2017-10-31 01:10:20 +0100

    Adding building graph routine:

---
+++ scripts/global_influence_ranking/BuildGraph.R
@@ -0,0 +1,8 @@
+library(igraph)
+
+BuildGraph <- function(filename = "../../results/TimeSeriesExperiments/results_2017-10-30_23-22-06_unity.csv") {
+  globalInfluence <- read.csv(filename)
+  g <- graph(directed = TRUE, edges = as.vector(t(globalInfluence[,1:2])))
+  deg <- degree(g, mode="out")
+  plot(g, vertex.size=deg*0.9, edge.arrow.size=.2, layout=layout_in_circle)
+}
